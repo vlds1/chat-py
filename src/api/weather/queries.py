53a876048e62f2a655b6b2e9d0ea4621c0eca9b1
Rodionov Kirill <k.rodionov@astondevs.ru>
2023-05-10 15:18:08 +0300

    new(pre-commit): added config

--- src/api/weather/queries.py
+++ src/api/weather/queries.py
@@ -1,6 +1,8 @@
 from enum import Enum
 
-from ariadne import gql, make_executable_schema, ObjectType, load_schema_from_path
+from ariadne import load_schema_from_path
+from ariadne import make_executable_schema
+from ariadne import ObjectType
 from ariadne.asgi import GraphQL
 
 from src.api.weather.crud import get_latest_record
@@ -10,25 +12,26 @@ type_defs = load_schema_from_path("src/api/weather/schema.graphql")
 
 
 class Cities(Enum):
-
-    MOSCOW = 'Moscow'
-    ROSTOV_ON_DON = 'Rostov-on-Don'
-    BERLIN = 'Berlin'
-    WASHINGTON = 'Washington'
-    KIEV = 'Kyiv'
-    MINSK = 'Minsk'
-    PARIS = 'Paris'
+    MOSCOW = "Moscow"
+    ROSTOV_ON_DON = "Rostov-on-Don"
+    BERLIN = "Berlin"
+    WASHINGTON = "Washington"
+    KIEV = "Kyiv"
+    MINSK = "Minsk"
+    PARIS = "Paris"
 
 
 @query.field("GetCityWeather")
 async def resolve_get_city_weather(_, info, city="Moscow"):
     data = await get_latest_record(city)
-    print('data:', data)
+    print("data:", data)
     return data
 
 
-schema = make_executable_schema(type_defs, [query, ])
+schema = make_executable_schema(
+    type_defs,
+    [
+        query,
+    ],
+)
 graphql_app = GraphQL(schema, debug=True)
-
-
-
