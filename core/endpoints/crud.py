255cd0c7b31f24fc2671e376cea0959265a3b80a
user <user@users-Mac-mini.local>
2023-05-18 14:28:43 +0300

    redone-response

--- core/endpoints/crud.py
+++ core/endpoints/crud.py
@@ -2,6 +2,7 @@ import datetime
 
 import bcrypt
 import jwt
+from jwt import InvalidSignatureError
 from pydantic import ValidationError
 
 from core.database.db import users_collection
@@ -15,15 +16,15 @@ async def create_new_user(user_data: dict) -> dict:
         user_exists = users_collection.find_one(filter={"email": user_data["email"]})
 
         if user_exists:
-            return {"status": 409, "detail": "user already exists"}
+            return {"detail": "user already exists"}, 409
 
         user_data["password"] = bcrypt.hashpw(
             password=user_data["password"].encode(), salt=bcrypt.gensalt(rounds=12)
         )
         users_collection.insert_one(user_data)
-        return {"status": 201, "detail": "user has been registered"}
+        return {"detail": "user has been registered"}, 201
     except ValidationError as e:
-        return {"status": 400, "detail": e.errors()}
+        return {"detail": e.errors()}, 400
 
 
 async def login_user(user_data: dict) -> dict:
@@ -31,7 +32,7 @@ async def login_user(user_data: dict) -> dict:
         user_data = UserSchema(**user_data).dict()
         user_exists = users_collection.find_one(filter={"email": user_data["email"]})
         if not user_exists:
-            return {"status": 400, "detail": "user doesnt exists"}
+            return {"detail": "user doesnt exists"}, 400
         passwords_compare = bcrypt.checkpw(
             user_data["password"].encode(), user_exists["password"]
         )
@@ -42,22 +43,26 @@ async def login_user(user_data: dict) -> dict:
         refresh_token = await create_token(user_exists, "refresh", 24 * 60)
 
         return {
-            "status": 200,
-            "data": {"access_token": access_token, "refresh_token": refresh_token},
-        }
+            "data": {"access_token": access_token, "refresh_token": refresh_token}
+        }, 200
     except ValidationError as e:
-        return {"status": 400, "detail": e.errors()}
+        return {"err": e.__str__()}, 400
 
 
 async def update_access_token(data):
-    refresh_token_data = jwt.decode(
-        data["refresh_token"],
-        key="b37e50cedcd3e3f1ff64f4afc0422084ae694253cf399326868e07a35f4",
-        algorithms=["HS256"],
-    )
-    current_datetime = datetime.datetime.utcnow()
-    if current_datetime > datetime.datetime.fromtimestamp(refresh_token_data["exp"]):
-        return {"refresh_token": refresh_token_data}
+    try:
+        refresh_token_data = jwt.decode(
+            data["refresh_token"],
+            key="b37e50cedcd3e3f1ff64f4afc0422084ae694253cf399326868e07a35f4",
+            algorithms=["HS256"],
+        )
+        current_datetime = datetime.datetime.utcnow()
+        if current_datetime > datetime.datetime.fromtimestamp(
+            refresh_token_data["exp"]
+        ):
+            return {"refresh_token": refresh_token_data}
 
-    new_access_token = await create_token(refresh_token_data, "access", 10)
-    return {"status": 201, "access_token": new_access_token}
+        new_access_token = await create_token(refresh_token_data, "access", 10)
+        return {"access_token": new_access_token}, 201
+    except InvalidSignatureError as e:
+        return {"err": e.__str__()}, 400
--- core/endpoints/endpoints.py
+++ core/endpoints/endpoints.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from flask import jsonify
+from flask import make_response
 from flask import request
 
 from core.endpoints.crud import create_new_user
@@ -15,16 +15,16 @@ auth = Blueprint(
 @auth.route("/registration", methods=["POST"])
 async def registration():
     registration_res = await create_new_user(request.get_json())
-    return jsonify(registration_res)
+    return make_response(registration_res)
 
 
 @auth.route("/login", methods=["POST"])
 async def login():
     registration_res = await login_user(request.get_json())
-    return jsonify(registration_res)
+    return make_response(registration_res)
 
 
 @auth.route("/token/refresh", methods=["POST"])
 async def refresh_access_token():
     new_access_token = await update_access_token(request.get_json())
-    return jsonify(new_access_token)
+    return make_response(new_access_token)
