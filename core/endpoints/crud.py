c599c82fb398c122d9b0549bb8d23d81be4c447a
user <user@users-Mac-mini.local>
2023-05-19 09:32:15 +0300

    redone crud functions to class

--- core/endpoints/crud.py
+++ core/endpoints/crud.py
@@ -11,66 +11,70 @@ from core.endpoints.services import create_token
 from core.schemas.schemas import JWTSchema, UserSchema
 
 
-async def create_new_user(user_data: dict) -> tuple:
-    users_collection = get_users_collection()
-    try:
-        user_data = UserSchema(**user_data).dict()
-        user_exists = await users_collection.find_one(
-            filter={"email": user_data["email"]}
-        )
+class AuthUser:
+    def __init__(self):
+        self.users_collection = get_users_collection()
 
-        if user_exists:
-            return {"detail": "user already exists"}, 409
+    async def create_new_user(self, user_data: dict) -> tuple:
+        try:
+            user_data = UserSchema(**user_data).dict()
+            user_exists = await self.users_collection.find_one(
+                filter={"email": user_data["email"]}
+            )
 
-        user_data["password"] = bcrypt.hashpw(
-            password=user_data["password"].encode(), salt=bcrypt.gensalt(rounds=12)
-        )
-        await users_collection.insert_one(user_data)
-        return {"detail": "user has been registered"}, 201
-    except ValidationError as e:
-        return {"detail": e.errors()}, 400
+            if user_exists:
+                return {"detail": "user already exists"}, 409
 
+            user_data["password"] = bcrypt.hashpw(
+                password=user_data["password"].encode(), salt=bcrypt.gensalt(rounds=12)
+            )
+            await self.users_collection.insert_one(user_data)
+            return {"detail": "user has been registered"}, 201
+        except ValidationError as e:
+            return {"detail": e.errors()}, 400
 
-async def login_user(user_data: dict) -> tuple:
-    users_collection = get_users_collection()
-    try:
-        user_data = UserSchema(**user_data).dict()
-        user_exists = await users_collection.find_one(
-            filter={"email": user_data["email"]}
-        )
-        if not user_exists:
-            return {"detail": "user doesnt exists"}, 400
-        passwords_compare = bcrypt.checkpw(
-            user_data["password"].encode(), user_exists["password"]
-        )
-        if not passwords_compare:
-            raise ValidationError
+    async def login_user(self, user_data: dict) -> tuple:
+        try:
+            user_data = UserSchema(**user_data).dict()
+            user_exists = await self.users_collection.find_one(
+                filter={"email": user_data["email"]}
+            )
+            if not user_exists:
+                return {"detail": "user doesnt exists"}, 400
+            passwords_compare = bcrypt.checkpw(
+                user_data["password"].encode(), user_exists["password"]
+            )
+            if not passwords_compare:
+                raise ValidationError
 
-        access_token = await create_token(user_exists, "access", 10)
-        refresh_token = await create_token(user_exists, "refresh", 24 * 60)
+            access_token = await create_token(user_exists, "access", 10)
+            refresh_token = await create_token(user_exists, "refresh", 24 * 60)
 
-        return {
-            "data": {"access_token": access_token, "refresh_token": refresh_token}
-        }, 200
-    except ValidationError as e:
-        return {"detail": e.__str__()}, 400
+            return {
+                "data": {"access_token": access_token, "refresh_token": refresh_token}
+            }, 200
+        except ValidationError as e:
+            return {"detail": e.__str__()}, 400
 
+    async def update_access_token(self, refresh_token: dict) -> tuple:
+        try:
+            refresh_token = JWTSchema(**refresh_token).dict()
+            refresh_token_data = jwt.decode(
+                refresh_token["refresh_token"],
+                key=JWT_SECRET_KEY,
+                algorithms=["HS256"],
+            )
+            current_datetime = datetime.datetime.utcnow()
+            if current_datetime > datetime.datetime.fromtimestamp(
+                refresh_token_data["exp"]
+            ):
+                return {"refresh_token": refresh_token_data}, 200
 
-async def update_access_token(data: dict) -> tuple:
-    try:
-        refresh_token = JWTSchema(**data).dict()
-        refresh_token_data = jwt.decode(
-            refresh_token["refresh_token"],
-            key=JWT_SECRET_KEY,
-            algorithms=["HS256"],
-        )
-        current_datetime = datetime.datetime.utcnow()
-        if current_datetime > datetime.datetime.fromtimestamp(
-            refresh_token_data["exp"]
-        ):
-            return {"refresh_token": refresh_token_data}, 200
-
-        new_access_token = await create_token(refresh_token_data, "access", 10)
-        return {"access_token": new_access_token}, 201
-    except (InvalidSignatureError, ValidationError, jwt.exceptions.DecodeError) as e:
-        return {"detail": e.__str__()}, 400
+            new_access_token = await create_token(refresh_token_data, "access", 10)
+            return {"access_token": new_access_token}, 201
+        except (
+            InvalidSignatureError,
+            ValidationError,
+            jwt.exceptions.DecodeError,
+        ) as e:
+            return {"detail": e.__str__()}, 400
--- core/endpoints/endpoints.py
+++ core/endpoints/endpoints.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, make_response, request
 
-from core.endpoints.crud import create_new_user, login_user, update_access_token
+from core.endpoints.crud import AuthUser
 
 auth = Blueprint(
     "authentication",
@@ -10,17 +10,20 @@ auth = Blueprint(
 
 @auth.route("/registration", methods=["POST"])
 async def registration():
-    registration_res = await create_new_user(request.get_json())
+    user = AuthUser()
+    registration_res = await user.create_new_user(request.get_json())
     return make_response(registration_res)
 
 
 @auth.route("/login", methods=["POST"])
 async def login():
-    login_res = await login_user(request.get_json())
+    user = AuthUser()
+    login_res = await user.login_user(request.get_json())
     return make_response(login_res)
 
 
 @auth.route("/token/refresh", methods=["POST"])
 async def refresh_access_token():
-    new_access_token = await update_access_token(request.get_json())
+    user = AuthUser()
+    new_access_token = await user.update_access_token(request.get_json())
     return make_response(new_access_token)
