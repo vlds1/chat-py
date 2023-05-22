f0f21a86f3648f0e9514cd43f305957bab55bc54
user <user@users-Mac-mini.local>
2023-05-22 09:59:05 +0300

    refactored

--- core/namespaces/namespaces.py
+++ core/namespaces/namespaces.py
@@ -2,27 +2,23 @@ import uuid
 
 import socketio
 
-from core.services.chat_services import get_room_id
-
 
 class DefaultNameSpace(socketio.AsyncNamespace):
+    def __init__(self, *args, **kwargs):
+        self.room_id = uuid.uuid4()
+        super().__init__(*args, **kwargs)
+
     async def on_connect(self, sid: str, environ: dict) -> None:
-        print(f"connected, id: {sid}")
+        self.enter_room(sid, self.room_id)
+        print(f'{sid} connected to room: {self.room_id}')
 
     async def on_disconnect(self, sid: str) -> None:
-        print(f"disconnected {sid}")
-
-    async def on_join_room(self, sid: str, data: dict) -> None:
-        try:
-            room_id = uuid.uuid4()
-            self.enter_room(sid, room_id)
-        except Exception as e:
-            print(e)
+        self.leave_room(sid, self.room_id)
+        print(f'{sid} disconnected from room: {self.room_id}')
 
     async def on_message(self, sid: str, data: dict) -> None:
         try:
             message = data["message"]
-            room_id = get_room_id(data)
-            await self.emit("message", message, room=room_id, skip_sid=sid)
+            await self.emit("message", message, room=self.room_id, skip_sid=sid)
         except Exception as e:
             print(e)
