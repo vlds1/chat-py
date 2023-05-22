9e3f0b07970dc720ff04f2bdfde33797bb2698f1
user <user@users-Mac-mini.local>
2023-05-22 17:37:56 +0300

    redone config class

--- core/config.py
+++ core/config.py
@@ -1,25 +1,24 @@
 import os
 
 from dotenv import load_dotenv
+from pydantic import BaseSettings
 
 load_dotenv()
+
 JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
 
 
-class BaseConfig:
-    def __init__(self):
-        self.DB_NAME = None
-        self.DB_HOST = None
-        self.DB_PORT = None
-        self.DB_URL = None
+class DatabaseConfig(BaseSettings):
+    class Config:
+        env_file = ".env"
 
-    def db_conf(self):
-        load_dotenv()
-        self.DB_NAME = os.environ.get("DB_NAME")
-        self.DB_HOST = os.environ.get("DB_HOST")
-        self.DB_PORT = os.environ.get("DB_PORT")
-        self.DB_URL = os.environ.get("DB_URL")
-        return self
+    DB_NAME = os.environ.get("DB_NAME")
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_PORT = os.environ.get("DB_PORT")
+    DB_URL = os.environ.get("DB_URL")
 
 
-config = BaseConfig()
+def get_config():
+    load_dotenv()
+    _config = DatabaseConfig()
+    return _config
--- core/database/db.py
+++ core/database/db.py
@@ -1,13 +1,12 @@
 import motor.motor_asyncio
 
-from core.config import config
+from core.config import get_config
 
 
 def get_users_collection():
-    db_conf = config.db_conf()
-
-    client = motor.motor_asyncio.AsyncIOMotorClient(db_conf.DB_URL)
-    database = client[db_conf.DB_NAME]
+    config = get_config()
+    client = motor.motor_asyncio.AsyncIOMotorClient(config.DB_URL)
+    database = client[config.DB_NAME]
     users_collection = database.get_collection("users")
 
     return users_collection
